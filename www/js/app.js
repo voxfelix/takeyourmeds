284f8a7de70d127601a41b0e2648cf335a55b6c4
jackfriend <jackwillamfriend@gmail.com>
2016-03-26 23:29:42 -0400

    contacts is done

--- www/js/app.js
+++ www/js/app.js
@@ -1,85 +1,85 @@
 var app = angular.module('starter', ['ionic', 'ngCordova', 'ui.router']);
 
 app.run(function($ionicPlatform, $rootScope, $http) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+	$ionicPlatform.ready(function() {
+		if (window.cordova && window.cordova.plugins.Keyboard) {
 
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+			// Don't remove this line unless you know what you are doing. It stops the viewport
+			// from snapping when text inputs are focused. Ionic handles this internally for
+			// a much nicer keyboard experience.
+			cordova.plugins.Keyboard.disableScroll(true);
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
 
-    });
+	});
 });
 
-app.config(function ($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/');
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/');
 
-  $stateProvider
-    // for main page
-    .state('app', {
-      cache: false,
-      url: '/',
-      templateUrl: 'templates/home.html',
-      controller: 'index'
-    })
-    // for addPill view
-    .state('addPill', {
-      cache: false,
-      url: '/addPill',
-      templateUrl: 'templates/addPill.html',
-      controller: 'addPill',
-    })
-    // for editPill view
-    .state('editPill', {
-      cache: false,
-      url: '/editPill/:id',
-      templateUrl: 'templates/editPill.html',
-      controller: 'editPill',
-    })
-    // for contact
-    .state('contact', {
-      cache: false,
-      url: '/contact',
-      templateUrl: 'templates/contact.html',
-      controller: 'editPill',
-    })
+	$stateProvider
+	// for main page
+		.state('app', {
+			cache: false,
+			url: '/',
+			templateUrl: 'templates/home.html',
+			controller: 'index'
+		})
+		// for addPill view
+		.state('addPill', {
+			cache: false,
+			url: '/addPill',
+			templateUrl: 'templates/addPill.html',
+			controller: 'addPill',
+		})
+		// for editPill view
+		.state('editPill', {
+			cache: false,
+			url: '/editPill/:id',
+			templateUrl: 'templates/editPill.html',
+			controller: 'editPill',
+		})
+		// for contact
+		.state('contact', {
+			cache: false,
+			url: '/contact',
+			templateUrl: 'templates/contact.html',
+			controller: 'editPill',
+		})
 
 });
 
 
 // for the noSQL db - medications
 app.factory('pouchdb', function() {
-  return new PouchDB('appDB'); // to use the DB
+	return new PouchDB('appDB'); // to use the DB
 
-  /* Uncomment these lines to delete DB
-  db = new PouchDB('appDB');
-  db.destroy().then(function (){
-    console.log('db destroyed');
-  }, function(){
-    console.log('error destroying db');
-  });
-  */
+	/* Uncomment these lines to delete DB
+	db = new PouchDB('appDB');
+	db.destroy().then(function (){
+	  console.log('db destroyed');
+	}, function(){
+	  console.log('error destroying db');
+	});
+	*/
 
 });
 
 // for the noSQL db - contact
 app.factory('contactdb', function() {
-  return new PouchDB('contactdb'); // to use the DB
+	return new PouchDB('contactdb'); // to use the DB
 
-  /* Uncomment these lines to delete DB
-  db = new PouchDB('appDB');
-  db.destroy().then(function (){
-    console.log('db destroyed');
-  }, function(){
-    console.log('error destroying db');
-  });
-  */
+	/* Uncomment these lines to delete DB
+	db = new PouchDB('appDB');
+	db.destroy().then(function (){
+	  console.log('db destroyed');
+	}, function(){
+	  console.log('error destroying db');
+	});
+	*/
 
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,53 +1,57 @@
-app.controller('index', function ($scope, $state, $stateParams, $ionicActionSheet, $cordovaSms, pouchdb, contactdb) {
-  contactdb.put({
-    _id: 'contact',
-    number: '9145257507'
-  }).then(function() {
-    // alert('good');
-  }, function () {
-    // alert('bad');
-  });
+app.controller('index', function($scope, $state, $stateParams, $ionicActionSheet, $cordovaSms, pouchdb, contactdb) {
+	contactdb.put({
+		_id: 'contact',
+		number: '9145257507'
+	}).then(function() {
+		// alert('good');
+	}, function() {
+		// alert('bad');
+	});
 
-  // get medications from PouchDB
-  $scope.Medications = [];
-  pouchdb.allDocs({include_docs: true}).then(function (result) {
-    for (var i = 0; i < result.rows.length; i++) { // for var in object.rows
-      $scope.Medications.push(result.rows[i].doc);
-    };
+	// get medications from PouchDB
+	$scope.Medications = [];
+	pouchdb.allDocs({
+		include_docs: true
+	}).then(function(result) {
+		for (var i = 0; i < result.rows.length; i++) { // for var in object.rows
+			$scope.Medications.push(result.rows[i].doc);
+		};
 
-    $scope.$apply();
-    console.log($scope.Medications);
-  });
+		$scope.$apply();
+		console.log($scope.Medications);
+	});
 
-  // get contact details
-  $scope.contact = {};
-  contactdb.allDocs({include_docs: true}).then(function (result) {
-    $scope.contact = result.rows[0].doc;
-    $scope.$apply();
-    console.log($scope.contact);
-  });
+	// get contact details
+	$scope.contact = {};
+	contactdb.allDocs({
+		include_docs: true
+	}).then(function(result) {
+		$scope.contact = result.rows[0].doc;
+		$scope.$apply();
+		console.log($scope.contact);
+	});
 
-  $scope.sendSms = function () {
+	$scope.sendSms = function() {
 
-    var options = {
-      replaceLineBreaks: false, // true to replace \n by a new line, false by default
-      android: {
-        intent: '' // send SMS with the native android SMS messaging
-      }
-    };
+		var options = {
+			replaceLineBreaks: false, // true to replace \n by a new line, false by default
+			android: {
+				intent: '' // send SMS with the native android SMS messaging
+			}
+		};
 
-   $scope.textMessage = 'test message';
+		$scope.textMessage = 'test message';
 
-    $cordovaSms
-      .send($scope.contact.number, $scope.textMessage, options)
-      .then(function() {
-        alert('Success');
-        console.log('Success! SMS was sent');
-      }, function(error) {
-        alert('Error');
-        console.log('An error occurred');
-      });
-    };
+		$cordovaSms
+			.send($scope.contact.number, $scope.textMessage, options)
+			.then(function() {
+				alert('Success');
+				console.log('Success! SMS was sent');
+			}, function(error) {
+				alert('Error');
+				console.log('An error occurred');
+			});
+	};
 });
 
 
@@ -58,63 +62,65 @@ app.controller('index', function ($scope, $state, $stateParams, $ionicActionShee
 
 
 
+app.controller('addPill', function($scope, $state, $stateParams, $ionicActionSheet, $cordovaSms, pouchdb, contactdb) {
 
+	// get medications from PouchDB
+	$scope.Medications = [];
+	pouchdb.allDocs({
+		include_docs: true
+	}).then(function(result) {
+		for (var i = 0; i < result.rows.length; i++) { // for var in object.rows
+			$scope.Medications.push(result.rows[i].doc);
+		};
+		$scope.$apply();
+		console.log($scope.Medications);
 
-app.controller('addPill', function ($scope, $state, $stateParams, $ionicActionSheet, $cordovaSms, pouchdb, contactdb) {
+		// $scope.getId([array of objects])
+		$scope.getId = function(array) {
+			var list = [];
+			if (array.length == 0) {
+				list.push(parseInt(1110));
+			} else {
+				for (var i = 0; i < array.length; i++) {
+					list.push(parseInt(array[i]._id));
+				};
+			}
+			var val = (Math.max.apply(Math, list) + 1).toString();
+			return val;
+		};
 
-  // get medications from PouchDB
-  $scope.Medications = [];
-  pouchdb.allDocs({include_docs: true}).then(function (result) {
-    for (var i = 0; i < result.rows.length; i++) { // for var in object.rows
-      $scope.Medications.push(result.rows[i].doc);
-    };
-    $scope.$apply();
-    console.log($scope.Medications);
+		// for medData object to put into $scope.submit
+		$scope.new_id = $scope.getId($scope.Medications);
 
-    // $scope.getId([array of objects])
-    $scope.getId = function(array) {
-      var list = [];
-      if (array.length == 0) {
-        list.push(parseInt(1110));
-      } else {
-        for (var i = 0; i < array.length; i++) {
-          list.push(parseInt(array[i]._id));
-        };
-      }
-      var val = (  Math.max.apply(Math, list) +  1   ).toString();
-      return val;
-    };
+		$scope.medData = {
+			_id: $scope.new_id,
+			id: $scope.new_id,
+			name: null,
+			amount: null,
+			type: null,
+			dateTime: null
+		};
 
-    // for medData object to put into $scope.submit
-    $scope.new_id = $scope.getId($scope.Medications);
+		// to submit a pill
+		$scope.submit = function() {
+			pouchdb.put(
+				$scope.medData
+			).then(function() {
+				console.log('PouchDB doc created with _id: ', $scope.medData._id);
+			}).catch(function(error) {
+				console.log('Error creating PouchDB doc');
+				console.log(error);
+			});
+		};
 
-    $scope.medData = {
-      _id: $scope.new_id,
-      id: $scope.new_id,
-      name: null,
-      amount: null,
-      type: null,
-      dateTime: null
-    };
+		$scope.back = function() {
+			$state.go('app', {}, {
+				reload: true
+			});
+		};
 
-    // to submit a pill
-    $scope.submit = function () {
-      pouchdb.put(
-        $scope.medData
-      ).then(function () {
-        console.log('PouchDB doc created with _id: ', $scope.medData._id);
-      }).catch(function (error) {
-        console.log('Error creating PouchDB doc');
-        console.log(error);
-      });
-    };
-
-    $scope.back = function () {
-      $state.go('app', {}, {reload: true});
-    };
-
-  }); // NOTE: These braces belong to pouchdb.allDocs({}).then(...)
-  // I don't know why this is, just go with it...
+	}); // NOTE: These braces belong to pouchdb.allDocs({}).then(...)
+	// I don't know why this is, just go with it...
 });
 
 
@@ -125,65 +131,67 @@ app.controller('addPill', function ($scope, $state, $stateParams, $ionicActionSh
 
 
 
+app.controller('editPill', function($scope, $state, $stateParams, $ionicActionSheet, $cordovaSms, pouchdb, contactdb) {
+	// get medications from PouchDB
+	$scope.Medications = [];
+	pouchdb.allDocs({
+		include_docs: true
+	}).then(function(result) {
+		for (var i = 0; i < result.rows.length; i++) { // for var in object.rows
+			$scope.Medications.push(result.rows[i].doc);
+		};
+		$scope.$apply();
 
+		// to open selected medication
+		$scope.current_med = $stateParams.id.toString();
+		for (var i = 0; i < $scope.Medications.length; i++) {
+			if ($scope.Medications[i].id == $scope.current_med) {
+				$scope.medData = $scope.Medications[i];
+				console.log($scope.medData._id);
+				$scope.id = $scope.medData.id;
+			};
+		};
 
-app.controller('editPill', function ($scope, $state, $stateParams, $ionicActionSheet, $cordovaSms, pouchdb, contactdb) {
-  // get medications from PouchDB
-  $scope.Medications = [];
-  pouchdb.allDocs({include_docs: true}).then(function (result) {
-    for (var i = 0; i < result.rows.length; i++) { // for var in object.rows
-      $scope.Medications.push(result.rows[i].doc);
-    };
-    $scope.$apply();
+		$scope.submit = function() {
+			pouchdb.put(
+				$scope.medData
+			).then(function(res) {
+				console.log('Updated ', $scope.medData.id, ' doc');
+			}, function(error) {
+				console.log('Failed updating doc');
+				console.log(error);
+			});
+		};
 
-    // to open selected medication
-    $scope.current_med = $stateParams.id.toString();
-    for (var i = 0; i < $scope.Medications.length; i++) {
-      if ($scope.Medications[i].id == $scope.current_med) {
-        $scope.medData = $scope.Medications[i];
-        console.log($scope.medData._id);
-        $scope.id = $scope.medData.id;
-      };
-    };
+		// Show the action sheet
+		$scope.showActionSheet = function() {
+			var hideSheet = $ionicActionSheet.show({
 
-    $scope.submit = function () {
-      pouchdb.put(
-        $scope.medData
-      ).then(function (res) {
-        console.log('Updated ', $scope.medData.id, ' doc');
-      }, function (error) {
-        console.log('Failed updating doc');
-        console.log(error);
-      });
-    };
+				buttons: [],
+				destructiveText: 'Delete',
+				cancelText: 'Cancel',
+				destructiveButtonClicked: function() {
+					console.log($scope.medData);
+					pouchdb.get($scope.medData._id).then(function(doc) {
+						return pouchdb.remove(doc);
+						console.log('Deleted doc ' + $scope.id);
+					});
+				},
+				buttonClicked: function(index) {
+					return true;
+				}
 
-    // Show the action sheet
-    $scope.showActionSheet = function() {
-     var hideSheet = $ionicActionSheet.show({
+			});
+		};
 
-         buttons: [],
-         destructiveText: 'Delete',
-         cancelText: 'Cancel',
-         destructiveButtonClicked: function() {
-           console.log($scope.medData);
-           pouchdb.get($scope.medData._id).then(function (doc) {
-              return pouchdb.remove(doc);
-              console.log('Deleted doc ' + $scope.id);
-            });
-          },
-         buttonClicked: function(index) {
-           return true;
-         }
+		$scope.addPill = function() {
+			$state.go('app', {}, {
+				reload: true
+			});
+		};
 
-       });
-     };
-
-     $scope.addPill = function () {
-       $state.go('app', {}, {reload: true});
-     };
-
-  }); // NOTE: These braces belong to pouchdb.allDocs({}).then(...)
-      // Before all Hell gets loose, just listen to my advice... LEAVE THE BRACES!!!
+	}); // NOTE: These braces belong to pouchdb.allDocs({}).then(...)
+	// Before all Hell gets loose, just listen to my advice... LEAVE THE BRACES!!!
 });
 
 
